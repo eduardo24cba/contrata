ba7673e1978ec31a7e2d936504a75e7049dced0d
eduardo24cba <eduardo.24.cba@gmail.com>
2023-10-23 00:46:15 -0300

    Add files via upload

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,16 +1,54 @@
-function changeColor(){
-    let ser = document.getElementById("servicios");
-    let nav = document.querySelector("nav");
+const navbar = document.querySelector("nav");
+const servicios = document.getElementById("servicios");
+const nosotros = document.getElementById("nosotros");
+const contacto = document.getElementById("contacto");
+
+
+function applyFunctions(){
+    checkSection();
                 
-    if(ser.getBoundingClientRect().top < nav.getBoundingClientRect().height){
-        if(nav.getAttribute("class").includes("navbarTheme"))return;
-            nav.classList.remove("navbarTransparent");
-            nav.classList.add("navbarTheme");
+    /*if(servicios.getBoundingClientRect().top < navbar.getBoundingClientRect().height){
+        if(navbar.getAttribute("class").includes("navbarTheme"))return;
+            navbar.classList.remove("navbarTransparent");
+            navbar.classList.add("navbarTheme");
         }
     else{
-        if(nav.getAttribute("class").includes("navbarTransparent"))return;
-            nav.classList.remove("navbarTheme");
-            nav.classList.add("navbarTransparent");
+        if(navbar.getAttribute("class").includes("navbarTransparent"))return;
+            navbar.classList.remove("navbarTheme");
+            navbar.classList.add("navbarTransparent");
+    }
+    */
+}
+function removeClass(links){
+    links.forEach((e)=> {if(e.getAttribute("class").includes("active"))e.classList.remove("active")});
+}
+function checkSection(){
+    let links = document.querySelectorAll("a");
+    let size = navbar.getBoundingClientRect().height + 30;
+    if(servicios.getBoundingClientRect().top < size && servicios.getBoundingClientRect().bottom > 10){
+        if(links[1].getAttribute("class").includes("active"))return;
+        removeClass(links);
+        links[1].classList.add("active");
+        console.log("caso 1")
+    }else if(nosotros.getBoundingClientRect().top < size && nosotros.getBoundingClientRect().bottom > 10){
+            if(links[2].getAttribute("class").includes("active"))return;
+            removeClass(links);
+            links[2].classList.add("active");
+            console.log("caso 2")
+    }else if(contacto.getBoundingClientRect().top < size && contacto.getBoundingClientRect().bottom > 10){
+            if(links[3].getAttribute("class").includes("active"))return;
+            removeClass(links);
+            links[3].classList.add("active");
+            console.log("caso 3")
+        }
+    else{
+        removeClass(links);
     }
 }
-window.addEventListener("scroll", changeColor);
+function checkFunctions(){
+    //if(servicios.getBoundingClientRect().top < navbar.getBoundingClientRect().height && !navbar.getAttribute("class").includes("navbarTheme")){navbar.classList.remove("navbarTransparent");navbar.classList.add("navbarTheme");}
+    checkSection();
+    console.log("caso 4")
+}
+window.addEventListener("scroll", applyFunctions);
+window.addEventListener("load", checkFunctions)
